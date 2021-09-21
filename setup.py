2bd8595e7f228ae28be701c7d134347cebcac568
AbhinavTuli <abhinav.tuli99@gmail.com>
2021-09-21 22:11:36 +0530

    fix setup

--- setup.py
+++ setup.py
@@ -7,36 +7,36 @@ import subprocess
 
 if sys.version_info[0] == 3 and sys.version_info[1] == 6:
     subprocess.run(
-        ["python", "shm/py36_clinic.py", "shm/posixshmem.c"]
+        ["python", "hub_shm/py36_clinic.py", "hub_shm/posixshmem.c"]
     )
 elif sys.version_info[0] == 3 and sys.version_info[1] == 7:
     subprocess.run(
-        ["python", "shm/py37_clinic.py", "shm/posixshmem.c"]
+        ["python", "hub_shm/py37_clinic.py", "hub_shm/posixshmem.c"]
     )
 else:
     raise ValueError("Must run on Python 3.6 or 3.7")
 
 
 linux_module = Extension(
-    "shm/_posixshmem",
+    "hub_shm/_posixshmem",
     define_macros=[
         ("HAVE_SHM_OPEN", "1"),
         ("HAVE_SHM_UNLINK", "1"),
         ("HAVE_SHM_MMAN_H", 1),
     ],
     libraries=["rt"],
-    sources=["shm/posixshmem.c"],
+    sources=["hub_shm/posixshmem.c"],
 )
 
 
 darwin_module = Extension(
-    "shm/_posixshmem",
+    "hub_shm/_posixshmem",
     define_macros=[
         ("HAVE_SHM_OPEN", "1"),
         ("HAVE_SHM_UNLINK", "1"),
         ("HAVE_SHM_MMAN_H", 1),
     ],
-    sources=["shm/posixshmem.c"],
+    sources=["hub_shm/posixshmem.c"],
 )
 
 
